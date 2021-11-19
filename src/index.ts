137d3d8c59facc3cca8e9e3922128e62f47220bd
AKochiev <akochiev@mtsbank.ru>
2021-11-19 18:13:17 +0300

    first lesson

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { renderSearchFormBlock } from './search-form.js';
+import { renderSearchStubBlock } from './search-results.js';
+import { renderUserBlock } from './user.js';
+import { renderToast } from './lib.js';
+
+const dateNow = new Date();
+const defStartDate = new Date(dateNow.setDate(dateNow.getDate() + 1))
+  .toISOString()
+  .split('T')[0];
+const defEndDate = new Date(dateNow.setDate(dateNow.getDate() + 3))
+  .toISOString()
+  .split('T')[0];
+
+window.addEventListener('DOMContentLoaded', () => {
+  renderUserBlock('Wade Warren', '/img/avatar.png', 0);
+  renderSearchFormBlock(defStartDate, defEndDate);
+  renderSearchStubBlock();
+  renderToast(
+    {
+      text: 'Это пример уведомления. Используйте его при необходимости',
+      type: 'success',
+    },
+    {
+      name: 'Понял',
+      handler: () => {
+        console.log('Уведомление закрыто');
+      },
+    }
+  );
+});
---
+++ src/search-form.ts
@@ -0,0 +1,45 @@
+import { renderBlock } from './lib.js';
+
+const minDate = new Date().toISOString().split('T')[0];
+const nextMonth = new Date(new Date().setMonth(new Date().getMonth() + 2));
+const maxDate = new Date(nextMonth.setDate(0)).toISOString().split('T')[0];
+
+export function renderSearchFormBlock(dateStart: string, dateEnd: string) {
+  renderBlock(
+    'search-form-block',
+    `
+    <form>
+      <fieldset class="search-filedset">
+        <div class="row">
+          <div>
+            <label for="city">Город</label>
+            <input id="city" type="text" disabled value="Санкт-Петербург" />
+            <input type="hidden" disabled value="59.9386,30.3141" />
+          </div>
+          <!--<div class="providers">
+            <label><input type="checkbox" name="provider" value="homy" checked /> Homy</label>
+            <label><input type="checkbox" name="provider" value="flat-rent" checked /> FlatRent</label>
+          </div>--!>
+        </div>
+        <div class="row">
+          <div>
+            <label for="check-in-date">Дата заезда</label>
+            <input id="check-in-date" type="date" value=${dateStart} min=${minDate} max=${maxDate} name="checkin" />
+          </div>
+          <div>
+            <label for="check-out-date">Дата выезда</label>
+            <input id="check-out-date" type="date" value=${dateEnd} min=${minDate} max=${maxDate} name="checkout" />
+          </div>
+          <div>
+            <label for="max-price">Макс. цена суток</label>
+            <input id="max-price" type="text" value="" name="price" class="max-price" />
+          </div>
+          <div>
+            <div><button>Найти</button></div>
+          </div>
+        </div>
+      </fieldset>
+    </form>
+    `
+  );
+}
